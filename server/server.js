01f0ac37ab5199c6af1cd74138d7a77ea83d25c8
Abdul-Muqeet-12 <159135111+Abdul-Muqeet-12@users.noreply.github.com>
2026-06-28 22:59:52 +0500

    Configure Express server

--- server/server.js
+++ server/server.js
@@ -2,3 +2,14 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import cookieParser from "cookie-parser";
+
+const app = express();
+const port = process.env.PROT || 4000;
+
+app.get("/", (req, res) => {
+  res.send("MERN Authentication API is running...");
+});
+
+app.listen(port, () => {
+  console.log(`Server strated on PORT:${port}`);
+});
