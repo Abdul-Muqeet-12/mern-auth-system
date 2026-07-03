23db8cfad377f370d01219d28c812714bfd4f2cb
Abdul-Muqeet-12 <159135111+Abdul-Muqeet-12@users.noreply.github.com>
2026-07-03 20:45:31 +0500

    Hash user password with bcrypt

---
+++ server/controllers/auth.controller.js
@@ -0,0 +1,27 @@
+import bcrypt, { hash } from "bcryptjs";
+import JsonWebTokenError from "jsonwebtoken";
+import userModel from "../models/user.model.js";
+
+const register = async (req, res) => {
+  const { name, email, password } = req.body;
+
+  if (!name || !email || !password) {
+    return res.json({ success: false, message: "Missing Details" });
+  }
+  try {
+    const existingUser = await userModel.findOne({ email });
+
+    if (existingUser) {
+      return res.json({ success: false, message: "User already exists" });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = new userModel({ name, email, password: hashedPassword });
+    await user.save();
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+};
+
+export default register;
