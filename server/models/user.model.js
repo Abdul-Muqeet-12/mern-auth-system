1d4788172f111dbcbe26e8b7f85d9edd6bc4adce
Abdul-Muqeet-12 <159135111+Abdul-Muqeet-12@users.noreply.github.com>
2026-07-03 16:57:00 +0500

    Create User model

---
+++ server/models/user.model.js
@@ -0,0 +1,45 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6,
+  },
+  verifyOtp: {
+    type: String,
+    default: "",
+  },
+  verifyOtpExpireAt: {
+    type: Number,
+    default: 0,
+  },
+  isAccountVerified: {
+    type: Boolean,
+    default: false,
+  },
+  resetOtp: {
+    type: String,
+    default: "",
+  },
+  resetOtpExpireAt: {
+    type: Number,
+    default: 0,
+  },
+});
+
+const userModel = mongoose.model("user", userSchema);
+
+export default userModel;
