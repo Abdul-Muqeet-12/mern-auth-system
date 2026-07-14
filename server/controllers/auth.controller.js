21cad66151e7d29ba7ae7eefab5b2be758b6f855
Abdul-Muqeet-12 <159135111+Abdul-Muqeet-12@users.noreply.github.com>
2026-07-14 17:11:40 +0500

    Add authentication status endpoint

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -229,3 +229,25 @@ export const verifyEmail = async (req, res) => {
     return res.status(500).json({ success: false, message: error.message });
   }
 };
+
+export const isAuthenticated = async (req, res) => {
+  try {
+    const user = await userModel
+      .findById(req.userId)
+      .select("-password -verifyOtp -verifyOtpExpireAt");
+
+    if (!user) {
+      return res
+        .status(404)
+        .json({ success: false, message: "User not found" });
+    }
+
+    return res.status(200).json({
+      success: true,
+      message: "User is authenticated",
+      user,
+    });
+  } catch (error) {
+    return res.status(500).json({ success: false, message: error.message });
+  }
+};
--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -1,5 +1,6 @@
 import express from "express";
 import {
+  isAuthenticated,
   login,
   logout,
   register,
@@ -15,5 +16,6 @@ router.post("/login", login);
 router.post("/logout", logout);
 router.post("/send-verify-otp", authMiddleware, sendVerifyOtp);
 router.post("/verify-account", authMiddleware, verifyEmail);
+router.post("/is-auth", authMiddleware, isAuthenticated);
 
 export default router;
